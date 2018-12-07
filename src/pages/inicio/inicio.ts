44bbc91360b6138e2a4eba8cfc864e8566066556
sofiapm2001 <al3201@cedi.local>
2018-12-07 14:43:26 -0600

    comentario de alexa

--- src/pages/inicio/inicio.ts
+++ src/pages/inicio/inicio.ts
@@ -77,17 +77,17 @@ export class InicioPage {
       nombre_p: " Happily ever after potion" ,
       imagen: " ..//assets/imgs/potion.jpg",
       precio: " $350 pesos",
-      disponible: "Si"
+      disponible: "9"
     },
     {
       id: 3,
-      name: "Sofia Perez",
+      name: "Villalobos",
       resenas:[
         {
-          username:'Charity Case',
-          avatar:"..//assets/imgs/girl1.png",
-          review: "Belleza divina, produce gases",
-          fecha_r:"15/11/18"
+          username:'LoboFeroz',
+          avatar:"..//assets/imgs/boy1.png",
+          review: "Muy real",
+          fecha_r:"10/06/18"
 
         }
       ],
@@ -96,7 +96,64 @@ export class InicioPage {
       nombre_p: " Dinosaurio Real" ,
       imagen: " ..//assets/imgs/dino.jpg",
       precio: " $950 pesos",
-      disponible: "Si"
+      disponible: "6"
+    },
+    {
+      id: 4,
+      name: "Alexa Arenas",
+      resenas:[
+        {
+          username:'Fiona',
+          avatar:"..//assets/imgs/girl1.png",
+          review: "Belleza divina, produce gases",
+          fecha_r:"15/11/18"
+
+        }
+      ],
+      fecha: "10/12/18",
+      avatar:"..//assets/imgs/girl1.png",
+      nombre_p: " Niño Gavin" ,
+      imagen: " ..//assets/imgs/gavin.jpg",
+      precio: " $1800 pesos",
+      disponible: "1"
+    },
+    {
+      id: 5,
+      name: "Villalobos",
+      resenas:[
+        {
+          username:'LoboFeroz',
+          avatar:"..//assets/imgs/boy1.png",
+          review: "Muy suavecito, muy recomendado para piel sensible",
+          fecha_r:"15/11/18"
+
+        }
+      ],
+      fecha: "10/12/18",
+      avatar:"..//assets/imgs/girl1.png",
+      nombre_p: " Papel Higenico" ,
+      imagen: " ..//assets/imgs/papel.jpg",
+      precio: " $100 pesos",
+      disponible: "40"
+    },
+    {
+      id: 6,
+      name: "Marifer",
+      resenas:[
+        {
+          username:'Niña Rosita',
+          avatar:"..//assets/imgs/girl1.png",
+          review: "fluffy aff",
+          fecha_r:"15/11/18"
+
+        }
+      ],
+      fecha: "10/12/18",
+      avatar:"..//assets/imgs/girl1.png",
+      nombre_p: " Unicornio" ,
+      imagen: " ..//assets/imgs/unicornio.jpg",
+      precio: " $500 pesos",
+      disponible: "40"
     },
 
 
